66961026f4310edeb30d4820c921c320aca36d02
SmileyJoe <cody.strugnell@gmail.com>
2016-10-17 22:25:33 +0200

    Commenting code

--- debug/src/main/java/za/co/smileyjoedev/lib/debug/Debug.java
+++ debug/src/main/java/za/co/smileyjoedev/lib/debug/Debug.java
@@ -11,16 +11,26 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * Class to make logging with logcat easier.
+ * <p>
+ * Takes a default log tag so it doesn't need to be set each time, each
+ * log call also takes a list of any type of object, so logging a {@link String} can easily be
+ * logged with a {@link Integer}, for example.
+ * <p>
+ * All objects are formatted based on their type so they are easier to read.
+ * <p>
+ * All logs can turned on and off in the Manifest file so they can be automatically turned off
+ * for release builds.
+ *
  * Created by cody on 2016/10/15.
  */
 
 public class Debug {
-
+    // Tag for internal logs //
     private static final String TAG = "smileyjoedev_debug";
 
     private static String LOG_NAME = "";
-    private static final String LINE_BREAK = "\n";
-    private static boolean ENABLED = true;
+    private static boolean ENABLED = false;
     private static ArrayList<Integer> TYPES = new ArrayList<Integer>();
 
     // Types //
@@ -32,12 +42,30 @@ public class Debug {
     public static final int ALL = 6;
     public static final int NONE = 7;
 
+    /**
+     * Initialize the library, this can be called in the {@link android.app.Application} class.
+     * <p>
+     * Checks the manifest meta data and sets the required data:
+     * <ul>
+     *     <li>@string/lib_debug_meta_tag - the tag that will be used by default</li>
+     *     <li>@string/lib_debug_meta_enabled - whether to enable the logs or not</li>
+     * </ul>
+     * <p>
+     * If the tags are not present, an error is logged and the logging wont be enabled by default.
+     *
+     * @param context the current context
+     */
     public static void init(Context context){
         try {
+            // The tags that are used in the manifest //
             String keyTag = context.getString(R.string.lib_debug_meta_tag);
             String keyEnabled = context.getString(R.string.lib_debug_meta_enabled);
+
+            // Get the info from the manifest //
             ApplicationInfo ai = context.getPackageManager().getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
             Bundle bundle = ai.metaData;
+
+            // Get the settings out of the manifest info //
             LOG_NAME = bundle.getString(keyTag);
             ENABLED = bundle.getBoolean(keyEnabled);
         } catch (PackageManager.NameNotFoundException e) {
@@ -47,44 +75,125 @@ public class Debug {
         }
     }
 
+    /**
+     * Check if logging is enabled or not.
+     *
+     * @return whether logs are enabled or not
+     */
     public static boolean isEnabled(){
         return ENABLED;
     }
 
+    /**
+     * Set the types of logs that can be logged, defaults to all types.
+     *
+     * @param types the types to allow to log
+     */
     public static void setTYPES(ArrayList<Integer> types) {
         Debug.TYPES = types;
     }
 
+    /**
+     * Wrapper for {@link Log#d(String, String)}
+     * <p>
+     * Takes an array of any object type, these can be mixed, formats and logs the params.
+     * <p>
+     * The following are used:
+     * <ul>
+     *     <li>Tag - the tag set in the manifest</li>
+     *     <li>Message - Formatted message from the params passed in</li>
+     * </ul>
+     *
+     * @param params array of items to be logged
+     */
     public static void d(Object... params) {
         if (Debug.isLogging(DEBUG)) {
             Log.d(LOG_NAME, Helper.handleMessage(params));
         }
     }
 
+    /**
+     * Wrapper for {@link Log#e(String, String)}
+     * <p>
+     * Takes an array of any object type, these can be mixed, formats and logs the params.
+     * <p>
+     * The following are used:
+     * <ul>
+     *     <li>Tag - the tag set in the manifest</li>
+     *     <li>Message - Formatted message from the params passed in</li>
+     * </ul>
+     *
+     * @param params array of items to be logged
+     */
     public static void e(Object... params) {
         if (Debug.isLogging(ERROR)) {
             Log.e(Debug.LOG_NAME, Helper.handleMessage(params));
         }
     }
 
+    /**
+     * Wrapper for {@link Log#i(String, String)}
+     * <p>
+     * Takes an array of any object type, these can be mixed, formats and logs the params.
+     * <p>
+     * The following are used:
+     * <ul>
+     *     <li>Tag - the tag set in the manifest</li>
+     *     <li>Message - Formatted message from the params passed in</li>
+     * </ul>
+     *
+     * @param params array of items to be logged
+     */
     public static void i(Object... params) {
         if (Debug.isLogging(INFO)) {
             Log.i(Debug.LOG_NAME, Helper.handleMessage(params));
         }
     }
 
+    /**
+     * Wrapper for {@link Log#v(String, String)}
+     * <p>
+     * Takes an array of any object type, these can be mixed, formats and logs the params.
+     * <p>
+     * The following are used:
+     * <ul>
+     *     <li>Tag - the tag set in the manifest</li>
+     *     <li>Message - Formatted message from the params passed in</li>
+     * </ul>
+     *
+     * @param params array of items to be logged
+     */
     public static void v(Object... params) {
         if (Debug.isLogging(VERBOSE)) {
             Log.v(Debug.LOG_NAME, Helper.handleMessage(params));
         }
     }
 
+    /**
+     * Wrapper for {@link Log#w(String, String)}
+     * <p>
+     * Takes an array of any object type, these can be mixed, formats and logs the params.
+     * <p>
+     * The following are used:
+     * <ul>
+     *     <li>Tag - the tag set in the manifest</li>
+     *     <li>Message - Formatted message from the params passed in</li>
+     * </ul>
+     *
+     * @param params array of items to be logged
+     */
     public static void w(Object... params) {
         if (Debug.isLogging(WARNING)) {
             Log.w(Debug.LOG_NAME, Helper.handleMessage(params));
         }
     }
 
+    /**
+     * Checks if the provided type is enabled to be logged.
+     *
+     * @param type the type being logged
+     * @return whether the type is enabled to be logged.
+     */
     private static boolean isLogging(int type) {
         boolean isLogging = false;
 
--- debug/src/main/java/za/co/smileyjoedev/lib/debug/Helper.java
+++ debug/src/main/java/za/co/smileyjoedev/lib/debug/Helper.java
@@ -4,13 +4,36 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
+ * Class for helper functions so they don't get in the way of external calls.
+ *
  * Created by cody on 2016/10/15.
  */
 
 public class Helper {
 
     private static final String LINE_BREAK = "\n";
+    private static final String TEXT_NULL = "--NULL--";
+    private static final String TEXT_UNKNOWN = "--UNKNOWN TYPE--";
+    private static final String TEXT_FALSE = "FALSE";
+    private static final String TEXT_TRUE = "TRUE";
+    private static final String TEXT_BLANK = "--BLANK--";
+    private static final String TEXT_DEFAULT_MESSAGE = "********************";
+    private static final String TEXT_GLUE = ": ";
 
+    /**
+     * Converts an object to a String formatted to be output with logcat.
+     * <p>
+     * The following defaults apply.
+     * <ul>
+     *     <li>null is output as '--NULL--'</li>
+     *     <li>any exception will output '--UNKNOWN TYPE--'</li>
+     *     <li>toString() is called on any object that is not specifically handled</li>
+     *     <li>if the resulting string is empty '--BLANK--' is returned to show something was there</li>
+     * </ul>
+     *
+     * @param obj the object to convert
+     * @return the formatted string of the object
+     */
     private static String convertToString(Object obj) {
         String message = "";
 
@@ -19,9 +42,9 @@ public class Helper {
                 message = (String) obj;
             } else if (obj instanceof Boolean) {
                 if ((Boolean) obj) {
-                    message = "TRUE";
+                    message = TEXT_TRUE;
                 } else {
-                    message = "FALSE";
+                    message = TEXT_FALSE;
                 }
             } else if (obj instanceof Integer) {
                 message = Integer.toString((Integer) obj);
@@ -50,39 +73,51 @@ public class Helper {
                 message += LINE_BREAK;
             } else if (obj.getClass().isArray()) {
                 try {
-                    // TODO: Find a way to hadle arrays of primitive type //
                     message = LINE_BREAK + Arrays.asList(obj).toString()
                             + LINE_BREAK;
                 } catch (Exception e) {
-                    message = "UNKNOWN TYPE";
+                    message = TEXT_UNKNOWN;
                 }
             } else if (obj == null) {
-                message = "--NULL--";
+                message = TEXT_NULL;
             } else {
                 try {
                     message = obj.toString();
                 } catch (Exception e) {
                     e.printStackTrace();
-                    message = "UNKNOWN TYPE";
+                    message = TEXT_UNKNOWN;
                 }
             }
         } catch (NullPointerException e) {
-            message = "--NULL--";
+            message = TEXT_NULL;
         }
 
         if (message.trim().equals("")) {
-            message = "--BLANK--";
+            message = TEXT_BLANK;
         }
 
         return message;
     }
 
+    /**
+     * Takes an undefined number of parameters and formats them into a String to be output
+     * via logcat.
+     * <p>
+     * The following rules apply
+     * <ul>
+     *     <li>All params are concatenated together with ': '</li>
+     *     <li>If there are no params a line of '*'s is returned</li>
+     * </ul>
+     *
+     * @param params list of objects to be combined to a single formatted string.
+     * @return string representation of the objects
+     */
     public static String handleMessage(Object... params) {
         String message = "";
         boolean first = true;
 
-        if (params.length == 0) {
-            message = "*******************";
+        if (params == null || params.length == 0) {
+            message = TEXT_DEFAULT_MESSAGE;
             return message;
         }
 
@@ -96,9 +131,10 @@ public class Helper {
             if (first) {
                 first = false;
             } else {
-                message += ": ";
+                message += TEXT_GLUE;
             }
 
+            // I don't remember why this happens ...
             if (param instanceof ArrayList || param instanceof Object[]) {
                 first = true;
             }
